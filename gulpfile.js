e3227f072becaa8ccd70d07408814a10df052bad
arturo-24 <arturo_cochea@hotmail.com>
2025-05-10 12:11:45 -0500

    avances_SaSS_header_Mixins

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,17 @@
-import{src, dest, watch } from "gulp";
+import { src, dest, watch } from "gulp";
 import * as dartSass from "sass";
 import gulpSass from "gulp-sass";
 
 const sass = gulpSass(dartSass);
 
-export function css(done) {
-    src("src/scss/app.scss")
-        .pipe( sass().on('error', sass.logError))
-        .pipe(dest("build/css"));
-           
-    done();
+// Tarea para compilar SCSS
+export function css( done ) {
+    return src("src/scss/app.scss")
+        .pipe( sass().on("error", sass.logError) )
+        .pipe( dest("build/css") );
 }
 
+// Tarea para escuchar cambios
 export function dev() {
-    watch("src/scss/app.scss", css);
-   
-
+    watch("src/scss/**/*.scss", css); 
 }
