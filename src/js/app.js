5671a171156ba9603a6f0087b1d416e530680b8c
arturo-24 <arturo_cochea@hotmail.com>
2025-06-18 21:21:05 -0500

    performance_web_festival

--- src/js/app.js
+++ src/js/app.js
@@ -24,9 +24,12 @@ function crearGaleria() {
 
     for (let i = 1; i <= CANTIDAD_IMAGENES; i++) {
         const imagen = document.createElement('IMG');
-        imagen.src = `src/img/gallery/full/${i}.jpg`;
+        imagen.loading = 'lazy';
+        imagen.width = '300';
+        imagen.height = '200';
+        imagen.src = `src/img/gallery/thumb/${i}.jpg`;
         imagen.alt = 'Imagen de Galeria'
-        
+
         
         // Event Handler
         imagen.onclick = function() {
