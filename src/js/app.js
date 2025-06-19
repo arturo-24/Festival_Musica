2524d9139511743502401030dc15f6df69dce9eb
arturo-24 <arturo_cochea@hotmail.com>
2025-06-19 16:41:31 -0500

    Proyecto_performance_done_festivalMusica

--- src/js/app.js
+++ src/js/app.js
@@ -23,13 +23,12 @@ function crearGaleria() {
     const galeria = document.querySelector('.galeria-imagenes');
 
     for (let i = 1; i <= CANTIDAD_IMAGENES; i++) {
-        const imagen = document.createElement('IMG');
-        imagen.loading = 'lazy';
-        imagen.width = '300';
-        imagen.height = '200';
-        imagen.src = `src/img/gallery/thumb/${i}.jpg`;
-        imagen.alt = 'Imagen de Galeria'
-
+        const imagen = document.createElement('PICTURE');
+        imagen.innerHTML = `
+            <source srcset="build/img/gallery/thumb/${i}.avif" type="image/avif">
+            <source srcset="build/img/gallery/thumb/${i}.webp" type="image/webp">
+            <img loading="lazy" width="200" height="300" src="build/img/gallery/thumb/${i}.jpg" alt="imagen galeria">
+        `;
         
         // Event Handler
         imagen.onclick = function() {
@@ -42,9 +41,13 @@ function crearGaleria() {
 }
 
 function mostrarImagen(i) {
-    const imagen = document.createElement('IMG');
+    const imagen = document.createElement('PICTURE');
     imagen.src = `src/img/gallery/full/${i}.jpg`;
-    imagen.alt = 'Imagen de Galeria'
+    imagen.innerHTML = `
+        <source srcset="build/img/gallery/full/${i}.avif" type="image/avif">
+        <source srcset="build/img/gallery/full/${i}.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/img/gallery/thumb/${i}.jpg" alt="imagen galeria">
+    `;
 
     // Generar Modal
     const modal = document.createElement('DIV');
